008e8a1f4f8fc3569379955ec035335346e3d861
Edward Hotchkiss <e@ingk.com>
2011-11-29 19:13:55 -0800

    layout the top 20 modules to batch test

---
+++ test/popular_modules.test.js
@@ -0,0 +1,35 @@
+
+var vows = require("vows");
+var assert = require("assert");
+
+var version = require("../lib/version");
+
+/*
+  Top 20 (current)
+  Most popular NodeJS Modules
+*/
+
+var popular = [
+  "underscore",
+  "coffee-script",
+  "request",
+  "optimist",
+  "connect",
+  "colors",
+  "async",
+  "uglify-js",
+  "socket.io",
+  "redis",
+  "jade",
+  "jsdom",
+  "mime",
+  "vows",
+  "commander",
+  "mongodb",
+  "ejs",
+  "node-uuid",
+  "stylus",
+  "mongoose"
+];
+
+/* EOF */
